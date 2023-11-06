1099be41cbfbe3a6054269bde61d9d3e113e0ab6
jensenzhang <hack@jensen-zhang.site>
2023-11-06 22:29:04 +0800

    Updated fts_utils.sh script
    Completely change the fts related configuration from REST APIs to direct
    DB operations.
    
    Signed-off-by: jensenzhang <hack@jensen-zhang.site>

--- .devcontainer/containernet-utils/fts_utils.sh
+++ .devcontainer/containernet-utils/fts_utils.sh
@@ -2,22 +2,34 @@
 
 COMPOSE_PROJECT=${COMPOSE_PROJECT:-fts3_devcontainer}
 RUCIO_NODE=${RUCIO_NODE:-rucio}
-FTS_NODE=${FTS_NODE:-fts}
+FTS_NODE=${FTS_NODE:-fts-dev}
 FTS_HOST=${FTS_HOST:-fts}
 FTSDB_NODE=${FTSDB_NODE:-ftsdb}
 
 init_fts () {
     echo $FTS_NODE
-    docker-compose -p $COMPOSE_PROJECT exec $RUCIO_NODE xrdgsiproxy init -bits 2048 -valid 9999:00 -cert /opt/rucio/etc/usercert.pem  -key /opt/rucio/etc/userkey.pem
-    docker-compose -p $COMPOSE_PROJECT exec $RUCIO_NODE fts-rest-whoami -v -s https://$FTS_HOST:8446
-    docker-compose -p $COMPOSE_PROJECT exec $RUCIO_NODE fts-rest-delegate -vf -s https://$FTS_HOST:8446 -H 9999
+    # docker-compose -p $COMPOSE_PROJECT exec $RUCIO_NODE xrdgsiproxy init -bits 2048 -valid 9999:00 -cert /opt/rucio/etc/usercert.pem  -key /opt/rucio/etc/userkey.pem
+    docker-compose -p $COMPOSE_PROJECT exec $FTS_NODE fts-rest-whoami -v -s https://$FTS_HOST:8446
+    docker-compose -p $COMPOSE_PROJECT exec $FTS_NODE fts-rest-delegate -vf -s https://$FTS_HOST:8446 -H 9999
+}
+
+connect_fts () {
+    docker-compose -p $COMPOSE_PROJECT exec $FTS_NODE bash
+}
+
+dump_fts_log () {
+    docker-compose -p $COMPOSE_PROJECT exec $FTS_NODE bash -c "cat /var/log/fts3/fts3server.log"
+}
+
+connect_ftsdb () {
+    docker-compose -p $COMPOSE_PROJECT exec $FTSDB_NODE bash -c 'MYSQL_PWD=fts mysql -u fts fts'
 }
 
 gen_test_file () {
     local target_se=$1
     local filename=$2
     local filesize=$3
-    docker exec -ti $target_se mkdir -p /rucio 
+    docker exec -ti $target_se mkdir -p /rucio
     docker exec -ti $target_se dd if=/dev/urandom of=/rucio/$filename bs=1M count=$filesize
 }
 
@@ -25,7 +37,7 @@ submit_test_transfers () {
     local source_se=$1
     local dest_se=$2
     local filename=$3
-    docker-compose -p $COMPOSE_PROJECT exec $RUCIO_NODE fts-rest-transfer-submit -v -s https://$FTS_HOST:8446 root://$source_se//rucio/$filename root://$dest_se//rucio/$filename
+    docker-compose -p $COMPOSE_PROJECT exec $FTS_NODE fts-rest-transfer-submit -v -s https://$FTS_HOST:8446 root://$source_se//rucio/$filename root://$dest_se//rucio/$filename
 }
 
 clean_up_storage () {
@@ -66,6 +78,16 @@ dump_optimizer_hist () {
     docker-compose -p $COMPOSE_PROJECT exec $FTSDB_NODE bash -c 'echo "'"$sql_stmt"'" | mysql -u fts --password=fts fts'
 }
 
+dump_transfer_hist () {
+    local sql_stmt='select * from t_file where start_time > '"'"$1"'"
+    docker-compose -p $COMPOSE_PROJECT exec $FTSDB_NODE bash -c 'echo "'"$sql_stmt"'" | MYSQL_PWD=fts mysql -u fts fts'
+}
+
+dump_job_hist () {
+    local sql_stmt='select * from t_file'
+    docker-compose -p $COMPOSE_PROJECT exec $FTSDB_NODE bash -c 'echo "'"$sql_stmt"'" | MYSQL_PWD=fts mysql -u fts fts'
+}
+
 config_fts_link () {
     set -f
     local source_se=$1
@@ -73,8 +95,29 @@ config_fts_link () {
     local min_active=$3
     local max_active=$4
     local opt_mode=${5:-2}
-    local msg='{"symbolicname":"'$source_se'-'$dest_se'","source":"'$source_se'","destination":"'$dest_se'","min_active":'$min_active',"max_active":'$max_active',"optimizer_mode":'$opt_mode'}'
-    docker-compose -p $COMPOSE_PROJECT exec $RUCIO_NODE curl --capath /etc/grid-security/certificates -E /tmp/x509up_u0 --cacert /tmp/x509up_u0 -H "Content-Type: application/json" -d "$msg" https://fts:8446/config/links
+    local sql_stmt="insert into t_link_config (source_se, dest_se, symbolic_name, min_active, max_active, optimizer_mode) values ('$source_se', '$dest_se', '$source_se-$dest_se', $min_active, $max_active, $opt_mode)"
+    sql_stmt="$sql_stmt; select * from t_link_config"
+    docker-compose -p $COMPOSE_PROJECT exec $FTSDB_NODE bash -c 'MYSQL_PWD=fts mysql -u fts -e "'"$sql_stmt"'" fts'
+    set +f
+}
+
+update_fts_link () {
+    set -f
+    local source_se=$1
+    local dest_se=$2
+    local min_active=$3
+    local max_active=$4
+    local opt_mode=${5:-2}
+    local sql_stmt="update t_link_config set min_active='$min_active', max_active='$max_active', optimizer_mode='$opt_mode' where source_se='$source_se' AND dest_se='$dest_se'"
+    sql_stmt="$sql_stmt; select * from t_link_config"
+    docker-compose -p $COMPOSE_PROJECT exec $FTSDB_NODE bash -c 'MYSQL_PWD=fts mysql -u fts -e "'"$sql_stmt"'" fts'
+    set +f
+}
+
+show_fts_link_config () {
+    set -f
+    local sql_stmt="select * from t_link_config"
+    docker-compose -p $COMPOSE_PROJECT exec $FTSDB_NODE bash -c 'MYSQL_PWD=fts mysql -u fts -e "'"$sql_stmt"'" fts'
     set +f
 }
 
@@ -83,6 +126,6 @@ config_optimizer () {
     local dest_se=$2
     local active=$3
     local msg='{"source_se":"'$source_se'","dest_se":"'$dest_se'","active":'$active'}'
-    docker-compose -p $COMPOSE_PROJECT exec $RUCIO_NODE curl --capath /etc/grid-security/certificates -E /tmp/x509up_u0 --cacert /tmp/x509up_u0 -H "Content-Type: application/json" -d "$msg" https://fts:8446/optimizer/current
+    docker-compose -p $COMPOSE_PROJECT exec $FTS_NODE curl --capath /etc/grid-security/certificates -H "Content-Type: application/json" -d "$msg" https://fts:8446/optimizer/current
 }
 
